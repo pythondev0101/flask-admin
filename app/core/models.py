2ecc921d941142a9aad0708a7638550275ad22e5
Roberto Montemayor <rmontemayor0101@gmail.com>
2020-04-01 14:47:20 +0800

    Add Customer,City,Province to core models

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@
 from app import db
 from datetime import datetime
 
+
 # MODEL.BASE
 class Base(db.Model):
     __abstract__ = True
@@ -16,3 +17,23 @@ class Base(db.Model):
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
         self.active = 1
+
+
+class CoreCustomer(Base):
+    __abstract__ = True
+    fname = db.Column(db.String(64), nullable=False, server_default="")
+    lname = db.Column(db.String(64), nullable=False, server_default="")
+    phone = db.Column(db.String(64), nullable=False, server_default="")
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    zip = db.Column(db.Integer,nullable=False)
+    street = db.Column(db.String(64), nullable=False,server_default="")
+
+
+class CoreCity(Base):
+    __abstract__ = True
+    name = db.Column(db.String(64), nullable=False,server_default="")
+
+
+class CoreProvince(Base):
+    __abstract__ = True
+    name = db.Column(db.String(64), nullable=False,server_default="")
