338aecf23d5134db50b68c35d4d266de62922eaa
Robert Montemayor <36463571+pythondev0101@users.noreply.github.com>
2020-04-21 15:50:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class ProductionConfig(Config):
     """
     Production configurations
     """
-    SQLALCHEMY_DATABASE_URI = get_database()
+    SQLALCHEMY_DATABASE_URI = "mysql://pythondev0101:databasepassword@pythondev0101.mysql.pythonanywhere-services.com/pythondev0101$db_homebest"
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
