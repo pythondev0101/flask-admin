7c74b99948537b8054cef97c34c8d6361f56ac19
pythondev0101 <rmontemayor0101@gmail.com>
2020-05-07 16:51:35 +0800

    Add placeholder in AdminField

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -66,10 +66,16 @@ class AdminField(Field):
 
     widget = widgets.TextInput()
     
-    def __init__(self,input_type="text",model=None,*args, **kwargs):
+    def __init__(self,input_type="text",placeholder='',model=None,*args, **kwargs):
         super(AdminField,self).__init__(*args,**kwargs)
         self.label = kwargs.get('label')
         self.model = model
+        
+        if placeholder == '':
+            self.placeholder = self.label.upper()
+        else:
+            self.placeholder = placeholder
+
         if self.model:
             self.input_type = 'select'
         else:
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -167,9 +167,9 @@ def set_modal(url, form):
             if field.input_type == 'select':
                 data = field.model.query.all()
                 fields[row_count].append(
-                    {'name': field.name, 'label': field.label, 'type': field.input_type, 'data': data})
+                    {'name': field.name, 'label': field.label, 'type': field.input_type, 'data': data,'placeholder':field.placeholder})
             else:
-                fields[row_count].append({'name': field.name, 'label': field.label, 'type': field.input_type})
+                fields[row_count].append({'name': field.name, 'label': field.label, 'type': field.input_type,'placeholder':field.placeholder})
             field_count = field_count + 1
             js_fields.append(field.name)
         if field_count <= 2:
