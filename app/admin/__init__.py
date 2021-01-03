28cdf7fe427f84dabb0dcc60a370cb18c72cc88b
pythondev0101 <rmontemayor0101@gmail.com>
2021-01-03 21:11:09 +0800

    Update admin apis

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,7 +1,14 @@
-from flask import Blueprint,g
-bp_admin = Blueprint('bp_admin',__name__,template_folder='templates',static_folder='static')
+from flask import Blueprint
+
+
+
+bp_admin = Blueprint(
+    'bp_admin',__name__,template_folder='templates',
+    static_folder='static',static_url_path='/admin/static'
+    )
 
 blueprint_name = "bp_admin"  # The name of the module's blueprint
+
 module_name = "admin" # The name of the module
 
 # URLS DICTIONARY
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,23 +1,13 @@
 """ MODULE: ADMIN.ROUTES """
-""" FLASK IMPORTS """
-from flask import render_template, flash, redirect, url_for, request, current_app,g,jsonify
+from flask import render_template, flash, redirect, url_for, request, jsonify
 from flask_login import login_required, current_user
-"""--------------END--------------"""
-
-""" APP IMPORTS  """
+from flask_cors import cross_origin
+from sqlalchemy import text
+from app import CONTEXT, db
+from app.core.models import CoreModel, CoreModule
 from app.admin import bp_admin
 
-"""--------------END--------------"""
 
-""" TEMPLATES IMPORTS """
-from . import admin_templates
-"""--------------END--------------"""
-
-from app import CONTEXT
-from app.core.models import HomeBestModel,HomeBestModule
-from sqlalchemy import text
-from flask_cors import cross_origin
-from app import db
 
 # TODO: Para ito sa pag delete dito mag sstore index_url ng kung anong dinelete, 
 # issue to kapag rekta inopen yung edit page sa url address
@@ -32,9 +22,7 @@ def dashboard():
 @bp_admin.route('/apps')
 def apps():
     CONTEXT['active'] = 'apps'
-
-    modules = HomeBestModule.query.all()
-
+    modules = CoreModule.query.all()
     return render_template('admin/admin_apps.html',context=CONTEXT,title='Apps',modules=modules)
 
 
@@ -144,10 +132,10 @@ def admin_edit(form, update_url, oid, modal_form=False, action="admin/admin_edit
         CONTEXT['active'] = model_name
         delete_table = model.__tablename__
 
-    query1 = HomeBestModel.query.filter_by(name=model_name).first()
+    query1 = CoreModel.query.filter_by(name=model_name).first()
 
     if query1:
-        check_module = HomeBestModule.query.get(query1.module_id)
+        check_module = CoreModule.query.get(query1.module_id)
         CONTEXT['module'] = check_module.name
 
     if kwargs is not None:
@@ -165,96 +153,122 @@ def admin_edit(form, update_url, oid, modal_form=False, action="admin/admin_edit
                            action=action,extra_modal=extra_modal,index_url=index_url,title=form.edit_title,rendered_model=model)
 
 
-
-def admin_index(*model, fields, form=None, url='', action="admin/admin_actions.html",
+def admin_table(*model, fields, form=None, url='', action="admin/admin_actions.html",
                 create_modal="admin/admin_create_modal.html", view_modal="admin/admin_view_modal.html",
                 create_url="", edit_url="", template="admin/admin_table.html",kwargs=None):
     
     model_name = model[0].__amname__
-    if _check_read(model_name):
-        if kwargs is not None and 'models' in kwargs:
-            models = kwargs.get('models')
-        else:
-            if len(model) == 1:
-                models = model[0].query.with_entities(*fields).all()
-            elif len(model) == 2:
-                models = model[0].query.outerjoin(model[1]).with_entities(*fields).all()
-            elif len(model) == 3:
-                query1 = db.session.query(model[0],model[1],model[2])
-                models = query1.outerjoin(model[1]).outerjoin(model[2]).with_entities(*fields).all()
-
-        # TODO: check if Admin.model_name is implemented in the model
-        # Raise error if not
-
-        CONTEXT['create_modal']['title'] = model_name
-        CONTEXT['active'] = model_name
-        CONTEXT['model'] = model_name
-        query1 = HomeBestModel.query.filter_by(name=model_name).first()
-        if query1:
-            check_module = HomeBestModule.query.get(query1.module_id)
-            CONTEXT['module'] = check_module.name
-
-        table = model[0].__tablename__
-
-        global index_url
-        index_url = url
-
-        if kwargs is not None:
-            if 'template' in kwargs:
-                template = kwargs.get('template')
-            
-            if 'active' in kwargs:
-                CONTEXT['active'] = kwargs.get('active')
-            
-            if 'edit_url' in kwargs:
-                edit_url = kwargs.get('edit_url')
-            
-            if 'create_url' in kwargs:
-                create_url = kwargs.get('create_url')
-
-            # THIS IS FOR BOOLEAN WORD CONVERSION
-            # if 'convert_boolean' in kwargs:
-            #     if kwargs.get('convert_boolean') == 2:
-            #         for model in models:
-            #             for data in model:
-            #                 if data == True:
-            #                     data = 'YES'
-            #                 else:
-            #                     data = 'NO'
-
-        if form is not None:
-            table_fields = form.index_headers
-            title = form.title
-            index_title = form.index_title
-            index_message = form.index_message
-
-            if create_url and create_modal:
-                _set_modal(create_url, form)
-            elif create_modal:
-                _set_modal('', form)
-                
-        else:
-            if 'index_headers' not in kwargs:
-                raise NotImplementedError('Must implement index_headers')
-            else:
-                table_fields = kwargs.get('index_headers')
-            if 'index_title' not in kwargs:
-                raise NotImplementedError("Must implement index_title")
-            else:
-                index_title = kwargs.get('index_title')
-                title = index_title
-            if 'index_message' not in kwargs:
-                raise NotImplementedError("Must implement index_message")
-            else:
-                index_message = kwargs.get('index_message')
-
-        return render_template(template, context=CONTEXT,
-                            models=models, table_fields=table_fields,
-                            index_title=index_title, index_message=index_message,
-                            title=title, action=action, create_modal=create_modal,
-                            view_modal=view_modal, edit_url=edit_url,table=table,rendered_model=model[0])
-    else:
+    
+    if not _check_read(model_name):
         return render_template('auth/authorization_error.html',context=CONTEXT)
+
+    if kwargs is not None and 'models' in kwargs:
+        models = kwargs.get('models')
+    else:
+        if len(model) == 1:
+            models = model[0].query.with_entities(*fields).all()
+        elif len(model) == 2:
+            models = model[0].query.outerjoin(model[1]).with_entities(*fields).all()
+        elif len(model) == 3:
+            query1 = db.session.query(model[0],model[1],model[2])
+            models = query1.outerjoin(model[1]).outerjoin(model[2]).with_entities(*fields).all()
+
+    # TODO: check if Admin.model_name is implemented in the model
+    # Raise error if not
+
+    CONTEXT['create_modal'] = {'title': model_name}
+    CONTEXT['active'] = model_name
+    CONTEXT['model'] = model_name
+
+    query1 = CoreModel.query.filter_by(name=model_name).first()
+    if query1:
+        check_module = CoreModule.query.get(query1.module_id)
+        CONTEXT['module'] = check_module.name
+
+    table = model[0].__tablename__
+
+    global index_url
+    index_url = url
+
+    if kwargs is not None:
+        if 'template' in kwargs:
+            template = kwargs.get('template')
+        
+        if 'active' in kwargs:
+            CONTEXT['active'] = kwargs.get('active')
+        
+        if 'edit_url' in kwargs:
+            edit_url = kwargs.get('edit_url')
+        
+        if 'create_url' in kwargs:
+            create_url = kwargs.get('create_url')
+
+    if form is not None:
+        table_fields = form.index_headers
+        title = form.title
+        index_title = form.index_title
+        index_message = form.index_message
+
+        if create_url and create_modal:
+            fields = []
+            row_count = 0
+            field_sizes = []
+            js_fields = []
+
+            for row in form.create_fields():
+                fields.append([])
+                field_count = 0
+                for field in row:
+                    if field.input_type == 'select':
+                        data = field.model.query.all()
+                        # TODO: Dapat rektang AdminField nalang iaappend sa fields hindi na dictionary
+                        fields[row_count].append(
+                            {
+                                'name': field.name, 'label': field.label, 'type': field.input_type, 
+                                'data': data,'placeholder':field.placeholder,'required':field.required,'readonly':field.readonly,
+                                'auto_generated': field.auto_generated
+                                })
+                    else:
+                        fields[row_count].append(
+                            {
+                                'name': field.name, 'label': field.label, 'type': field.input_type,
+                                'placeholder':field.placeholder,'required':field.required,'readonly':field.readonly,
+                                'auto_generated': field.auto_generated
+                                })
+                    field_count = field_count + 1
+                    js_fields.append(field.name)
+                if field_count <= 2:
+                    field_sizes.append(6)
+                elif field_count >= 3:
+                    field_sizes.append(4)
+                row_count = row_count + 1
+
+            CONTEXT['create_modal'] = {
+                'create_url': url,
+                'fields_sizes':field_sizes,
+                'js_fields':js_fields
+            }
+
+    else:
+        if 'index_headers' not in kwargs:
+            raise NotImplementedError('Must implement index_headers')
+        else:
+            table_fields = kwargs.get('index_headers')
+        if 'index_title' not in kwargs:
+            raise NotImplementedError("Must implement index_title")
+        else:
+            index_title = kwargs.get('index_title')
+            title = index_title
+        if 'index_message' not in kwargs:
+            raise NotImplementedError("Must implement index_message")
+        else:
+            index_message = kwargs.get('index_message')
+    print(CONTEXT)
+    return render_template(template, context=CONTEXT, form=form, create_fields=fields,
+                        models=models, table_fields=table_fields,
+                        index_title=index_title, index_message=index_message,
+                        title=title, action=action, create_modal=create_modal,
+                        view_modal=view_modal, edit_url=edit_url,table=table,rendered_model=model[0])
 
 
 def _check_read(model_name):
@@ -273,60 +287,20 @@ def _check_read(model_name):
         return False
 
 
-def _set_modal(url, form):
-    fields = []
-    row_count = 0
-    field_sizes = []
-    js_fields = []
-    for row in form.create_fields():
-        fields.append([])
-        field_count = 0
-        for field in row:
-            if field.input_type == 'select':
-                data = field.model.query.all()
-                # TODO: Dapat rektang AdminField nalang iaappend sa fields hindi na dictionary
-                fields[row_count].append(
-                    {
-                        'name': field.name, 'label': field.label, 'type': field.input_type, 
-                        'data': data,'placeholder':field.placeholder,'required':field.required,'readonly':field.readonly,
-                        'auto_generated': field.auto_generated
-                        })
-            else:
-                fields[row_count].append(
-                    {
-                        'name': field.name, 'label': field.label, 'type': field.input_type,
-                        'placeholder':field.placeholder,'required':field.required,'readonly':field.readonly,
-                        'auto_generated': field.auto_generated
-                        })
-            field_count = field_count + 1
-            js_fields.append(field.name)
-        if field_count <= 2:
-            field_sizes.append(6)
-        elif field_count >= 3:
-            field_sizes.append(4)
-        row_count = row_count + 1
-    CONTEXT['create_modal'] = {
-        'create_url': url,
-        'create_form': form,
-        'fields': fields,
-        'fields_sizes':field_sizes,
-        'js_fields':js_fields
-    }
-
-
 def admin_dashboard(box1=None,box2=None,box3=None,box4=None):
     from app.auth.models import User
     if not box1:
-        box1 = DashboardBox("Total Modules","Installed",HomeBestModule.query.count())
+        box1 = DashboardBox("Total Modules","Installed",CoreModule.query.count())
 
     if not box2:
-        box2 = DashboardBox("System Models","Total models",HomeBestModel.query.count())
+        box2 = DashboardBox("System Models","Total models",CoreModel.query.count())
 
     if not box3:
         box3 = DashboardBox("Users","Total users",User.query.count())
     
     CONTEXT['active'] = 'main_dashboard'
     CONTEXT['module'] = 'admin'
+
     return render_template("admin/admin_dashboard.html", context=CONTEXT,title='Admin Dashboard', \
         box1=box1,box2=box2,box3=box3)
 
