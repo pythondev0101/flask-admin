68d74e40adae4ccdea1b86f47307032294cbcc0b
pythondev0101 <rmontemayor0101@gmail.com>
2021-01-26 18:40:45 +0800

    change bp_iwms

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     ADMIN = {
         'DATA_PER_PAGE': 25,
         'HOME_URL': 'bp_admin.dashboard',
-        'DASHBOARD_URL': 'bp_iwms.dashboard',
+        'DASHBOARD_URL': 'bp_admin.dashboard',
         'FIRST_SIDEBAR_HEADER': 'ADMINISTRATION',
         'SECOND_SIDEBAR_HEADER': 'SYSTEM MODELS'
     }
