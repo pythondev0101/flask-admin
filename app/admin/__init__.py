73b3fa095eccb077b5255c8a216caba8bc215746
pythondev0101 <rmontemayor0101@gmail.com>
2021-02-01 12:55:05 +0800

    to v1.6 and implement sidebar

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -21,6 +21,8 @@ admin_templates = {
     'index': 'admin/admin_dashboard.html',
 }
 
-from .templating import admin_render_template
+from . import templating
 from . import routes
 from . import models
+from . import views
+from . import notifications
--- app/admin/admin.py
+++ app/admin/admin.py
@@ -1,6 +1,7 @@
 from flask import current_app
-from app.auth.models import User,Role
 from app.core import CoreModule
+from .models import AdminDashboard, AdminApp
+from app.auth.models import User,Role
 
 
 
@@ -10,5 +11,13 @@ class AdminModule(CoreModule):
     module_link = current_app.config['ADMIN']['HOME_URL']
     module_short_description = 'Administration'
     module_long_description = "Administration Dashboard and pages"
-    models = [User, Role]
+    models = [AdminDashboard, AdminApp, User, Role]
     version = '1.0'
+    sidebar = {
+        'DASHBOARDS': [
+            AdminDashboard, AdminApp
+        ],
+        'SYSTEM MODELS': [
+            User, Role
+        ]
+    }
